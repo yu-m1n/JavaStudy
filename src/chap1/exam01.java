051c4f74f2deb5eb0cd8ea2e29130d495636698e
yumin <rdddq01@gmail.com>
2024-05-07 23:40:30 +0900

    혼공자 공부 기록

---
+++ src/chap1/exam01.java
@@ -0,0 +1,13 @@
+package chap1;
+/*
+작성자: 박유민
+작성일 2024.05.07
+*/
+
+public class exam01 {
+    //프로그램 실행 진입점
+    public static void main(String[] args){
+        //콘솔에 진행하는 실행문
+        System.out.println("Hello Java");
+    } //end of main
+} //end of class
