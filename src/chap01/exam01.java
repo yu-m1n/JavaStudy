a4d0035689a21f61c610191dedc64812a3cc06a3
yumin <rdddq01@gmail.com>
2024-05-08 21:31:31 +0900

    혼공자 공부 기록

--- src/chap01/exam01.java
+++ src/chap01/exam01.java
@@ -1,10 +1,10 @@
-package chap1;
+package chap01;
 /*
 작성자: 박유민
 작성일 2024.05.07
 */
 
-public class exam01 {
+public class exam01 { //Hello.java
     //프로그램 실행 진입점
     public static void main(String[] args){
         //콘솔에 진행하는 실행문
